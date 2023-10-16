12c13c8ff28381e046d2186626e414466edbd2ac
Michijoe <e2296540@cmaisonneuve.qc.ca>
2023-10-16 10:21:29 -0400

    modif home content

--- lang/fr/content.php
+++ lang/fr/content.php
@@ -2,7 +2,7 @@
 
 return [
     'home_title'        => 'Rejoignez le réseau social du NewLodge&nbsp;College !',
-    'home_intro'        => "Cette plateforme est exclusivement dédiée à notre dynamique cégep et propose un espace virtuel conçu spécialement pour faciliter la communication, le partage et la collaboration entre nos étudiants. Que vous soyez en première année ou en dernière année, ce réseau social a été pensé pour vous offrir une expérience immersive et enrichissante au sein de notre communauté académique. Découvrez ici la liste complète de nos étudiants inscrits, partagez vos documents et découvrez des articles captivants dans notre blog. Ensemble, bâtissons un environnement propice à l'apprentissage et à l'échange au sein de notre cégep.",
+    'home_intro'        => "Cette plateforme est exclusivement dédiée à notre dynamique cégep et propose un espace virtuel conçu spécialement pour faciliter la communication, le partage et la collaboration entre nos étudiants. Découvrez ici la liste complète de nos étudiants inscrits, partagez vos documents et découvrez des articles captivants dans notre blog. Ensemble, bâtissons un environnement propice à l'apprentissage et à l'échange au sein de notre cégep.",
     'email_reinit'      => ', voici le lien pour réinitialiser votre mot de passe.',
     'email_signature'   => "L'équipe sociale du NewLodge&nbsp;College.",
     'email_activation'  => 'Bienvenue sur le réseau social du NewLodge&nbsp;College !',
